442ebb154b756a1abb93624d8e24b0ba689a61b0
Maksym Protsyk <maksprotsyk@gmail.com>
2025-08-25 19:49:46 +0300

    + added Green Slime with animations

---
+++ Assets/Scripts/Projectiles/BaseProjectile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Characters;
+using UnityEngine;
+
+namespace Projectiles
+{
+    public class BaseProjectile : MonoBehaviour, IDamageWriter
+    {
+        private float _damage = 10f;
+
+        public void SetDamage(float value)
+        {
+            _damage = value;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            CharacterBase character = collision.GetComponent<CharacterBase>();
+            if (character)
+            {
+                character.Health.TakeDamage(_damage);
+                Debug.Log("Damaged someone for " + _damage);
+            }
+        }
+
+    }
+}
