580dbbb187b043ae736c692705f741d89c052aab
Maksym Protsyk <maksprotsyk@gmail.com>
2025-08-30 00:33:04 +0300

    + added more enemies

--- Assets/Scripts/DataStorage/Generated/Enemies.cs
+++ Assets/Scripts/DataStorage/Generated/Enemies.cs
@@ -3,7 +3,15 @@ namespace DataStorage.Generated
     [System.Serializable]
     public class Enemies: TableID
     {
-        public static readonly Enemies GreenSlime = new Enemies("GreenSlime");
+        public static readonly Enemies Slime1 = new Enemies("Slime1");
+        public static readonly Enemies Slime2 = new Enemies("Slime2");
+        public static readonly Enemies Slime3 = new Enemies("Slime3");
+        public static readonly Enemies Beholder1 = new Enemies("Beholder1");
+        public static readonly Enemies Beholder2 = new Enemies("Beholder2");
+        public static readonly Enemies Beholder3 = new Enemies("Beholder3");
+        public static readonly Enemies Vampire1 = new Enemies("Vampire1");
+        public static readonly Enemies Vampire2 = new Enemies("Vampire2");
+        public static readonly Enemies Vampire3 = new Enemies("Vampire3");
         public Enemies(string id): base(id){}
     }
 #if UNITY_EDITOR
